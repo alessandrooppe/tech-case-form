e15c1416227145a43e08c82097d2903290372e9a
alessandrooppe <alessandro.oppe@gmail.com>
2024-06-23 16:25:04 +0200

    Add step 3

--- app/components/steps/step3.tsx
+++ app/components/steps/step3.tsx
@@ -1,9 +1,12 @@
 // components/Step3.tsx
 export const Step3 = () => {
   return (
-    <div>
-      <h2 className="text-2xl">Step 3: Complete</h2>
-    </div>
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="bg-white p-8 rounded-lg shadow-lg text-center max-w-md">
+        <h1 className="text-2xl font-semibold text-green-600 mb-4">Successo!</h1>
+        <p className="text-gray-700">Hai completato il form e inviato i dati.</p>
+      </div>
+  </div>
   );
 };
 
